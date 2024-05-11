rpuboh/MediaWiki

commit 9bcd56a023f32167d9668943444c652f46568337

Remove redundant downstream fix after upgrading SMW
Also partially reverts commit e67870ecc0edeb5529839ef602470cce5d3d29a4

LocalSettings.php
@@ -509,15 +509,6 @@ $wgFooterIcons = [
 			"height" => "31",
 			"width" => "88",
 		],
-	],
-    "poweredby" => [
-		"semanticmediawiki" => [
-            'src' => "$wgResourceBasePath/resources/assets/SemanticMediaWiki.png",
-            'url' => 'https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki',
-            'alt' => 'Powered by Semantic MediaWiki',
-            "height" => "31",
-            "width" => "88",
-		]
 	]
 ];
 

commit b4cf12056f66de526915559c11437cdf8f8e1d37

Fix missing ['poweredby']['mediawiki'] footer icon
In Setup.php, the various $wg* global variables are first populated
using config-schema.php, then modified by LocalSettings.php.
After that SetupDynamicConfig.php is invoked to perform some checks,
including adding the MediaWiki icon in footer.

Keep the original $wgFooterIcons array to make upstream feature
function normally.

Fixes: 9bcd56a

LocalSettings.php
@@ -468,16 +468,14 @@ $wgContentNamespaces = [ 0, 300, ];
 $wgNamespacesToBeSearchedDefault[NS_FANMADE] = true;
 
 //页底
-$wgFooterIcons = [
-    "copyright" => [
-        "myicon" => [
-			"src" => "/resources/assets/cc-by-sa-button.png",
-			"url" => "https://creativecommons.org/licenses/by-sa/4.0/",
-			"alt" => "知识共享署名-相同方式共享 4.0",
-			"height" => "31",
-			"width" => "88",
-		],
-	]
+$wgFooterIcons["copyright"] = [
+	"myicon" => [
+		"src" => "/resources/assets/cc-by-sa-button.png",
+		"url" => "https://creativecommons.org/licenses/by-sa/4.0/",
+		"alt" => "知识共享署名-相同方式共享 4.0",
+		"height" => "31",
+		"width" => "88",
+	],
 ];
 
 $wgRightsIcon = null;